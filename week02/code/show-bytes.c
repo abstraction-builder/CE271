0ac097458a835bb7bc62ad595a48d8580b19b78a
Amiko Malania <amikomalania@gmail.com>
2023-10-02 21:00:04 +0400

    Done with Seminar 1

--- week02/code/show-bytes.c
+++ week02/code/show-bytes.c
@@ -100,27 +100,27 @@ void show_twocomp()
 
 int main(int argc, char *argv[])
 {
-    int val = 12345;
+    int val = 15213;
 
     if (argc > 1) {
-	if (argc > 1) {
-	    val = strtol(argv[1], NULL, 0);
-	}
-	printf("calling test_show_bytes\n");
-	test_show_bytes(val);
+        if (argc > 1) {
+            val = strtol(argv[1], NULL, 0);
+        }
+        printf("calling test_show_bytes\n");
+        test_show_bytes(val);
     } else {
-	printf("calling show_twocomp\n");
-	show_twocomp();
-	printf("Calling simple_show_a\n");
-	simple_show_a();
-	printf("Calling simple_show_b\n");
-	simple_show_b();
-	printf("Calling float_eg\n");
-	float_eg();
-	printf("Calling string_ueg\n");
-	string_ueg();
-	printf("Calling string_leg\n");
-	string_leg();
+        printf("calling show_twocomp\n");
+        show_twocomp();
+        printf("Calling simple_show_a\n");
+        simple_show_a();
+        printf("Calling simple_show_b\n");
+        simple_show_b();
+        printf("Calling float_eg\n");
+        float_eg();
+        printf("Calling string_ueg\n");
+        string_ueg();
+        printf("Calling string_leg\n");
+        string_leg();
     }
     return 0;
 }
