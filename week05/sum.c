ca7529ecb82d7f62a5e014213f6a246dcf516b20
abstraction-builder <amalania@qarva.com>
2023-10-23 19:03:33 +0400

    Done with Week 5 lecture

--- week05/sum.c
+++ week05/sum.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-
-#define FOO 5
-
 long mult2(long x, long y)
 {
 	return x * y; 
@@ -11,16 +7,4 @@ void multstore(long x, long y, long *dest)
 {
 	long t = mult2(x, y);
 	*dest = t;
-}
-
-int main(void) {
-	long x = 15;
-	long y = 213;
-	long t = 0;
-
-	multstore(x, y, &t);
-
-	printf("x * y = %ld\n", t);
-	
-	return 0;
 }
--- week05/sum.s
+++ week05/sum.s
@@ -1,52 +1,11 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 14, 0	sdk_version 14, 0
-	.globl	_mult2                          ; -- Begin function mult2
-	.p2align	2
-_mult2:                                 ; @mult2
-	.cfi_startproc
-; %bb.0:
-	mul	x0, x1, x0
+mult2:
+	movq	%rdi, %rax
+	imulq	%rsi, %rax
 	ret
-	.cfi_endproc
-                                        ; -- End function
-	.globl	_multstore                      ; -- Begin function multstore
-	.p2align	2
-_multstore:                             ; @multstore
-	.cfi_startproc
-; %bb.0:
-	mul	x8, x1, x0
-	str	x8, [x2]
+multstore:
+	pushq	%rbx
+	movq	%rdx, %rbx
+	call	mult2
+	movq	%rax, (%rbx)
+	popq	%rbx
 	ret
-	.cfi_endproc
-                                        ; -- End function
-	.globl	_main                           ; -- Begin function main
-	.p2align	2
-_main:                                  ; @main
-	.cfi_startproc
-; %bb.0:
-	sub	sp, sp, #32
-	.cfi_def_cfa_offset 32
-	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
-	add	x29, sp, #16
-	.cfi_def_cfa w29, 16
-	.cfi_offset w30, -8
-	.cfi_offset w29, -16
-	mov	w8, #3195
-	str	x8, [sp]
-Lloh0:
-	adrp	x0, l_.str@PAGE
-Lloh1:
-	add	x0, x0, l_.str@PAGEOFF
-	bl	_printf
-	mov	w0, #0
-	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
-	add	sp, sp, #32
-	ret
-	.loh AdrpAdd	Lloh0, Lloh1
-	.cfi_endproc
-                                        ; -- End function
-	.section	__TEXT,__cstring,cstring_literals
-l_.str:                                 ; @.str
-	.asciz	"x * y = %ld\n"
-
-.subsections_via_symbols
