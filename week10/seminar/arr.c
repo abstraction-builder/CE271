08d75c83f154972be7e04bc187f0a09e3601f6d0
Amiko Malania <amikomalania@gmail.com>
2023-12-03 12:43:22 +0400

    Add seminar code; Add presentation for week 11

---
+++ week10/seminar/arr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define SIZE 5
+#define INDEX -1
+
+int main()
+{
+	int arr[SIZE] = {10000, 2, 3, 4, 5};
+
+	printf("arr[%d] = %d\n", INDEX, arr[INDEX]);
+
+	return 0;
+}
---
+++ week10/seminar/foo.c
@@ -0,0 +1,6 @@
+int main()
+{
+	int arr[] = {1, 2, 3};
+
+	return 0;
+}
